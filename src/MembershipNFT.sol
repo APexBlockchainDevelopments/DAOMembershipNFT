cad2497648fd1456549d31c379e49d2be81b2d21
APex <37938428+APexBlockchainDevelopments@users.noreply.github.com>
2024-03-06 23:43:12 -0700

    more notes

--- src/MembershipNFT.sol
+++ src/MembershipNFT.sol
@@ -141,7 +141,7 @@ contract MembershipNFT is ERC721, Ownable {
         );
     }
 
-    function membershipStructUri() public view returns(string memory){  //used for testing and re-organizing json info, stll a work in progress
+    function membershipStructUri() public view returns(string memory){  //used for testing and re-organizing json info, stll a work in progress. Need to figure out how to import a struct and given enough freedom to DAO
         string memory description = '", "description":"A former member of the DAO! This NFT Proves it! This NFT changes when a member leaves the DAO.", ';
         address wallet;
         uint256 joinDate;
